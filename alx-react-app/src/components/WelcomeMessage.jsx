f9f4f803c2deb677d37bdc4a26f4984606d7c8be
issam-os <issam.bouyoussef94@gmail.com>
2025-02-16 23:47:46 +0100

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,11 @@
-function App() {
+function WelcomeMessage() {
     return (
         <div>
-            <WelcomeMessage />
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
 
-export default App;
+export default WelcomeMessage;
