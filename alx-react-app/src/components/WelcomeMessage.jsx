c39989b58676e684da48a679684796b91ee0d452
issam-os <issam.bouyoussef94@gmail.com>
2025-02-20 22:21:50 +0100

    Fix duplicate code issue in WelcomeMessage.jsx and correct App.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 function WelcomeMessage() {
@@ -5,8 +6,18 @@ function WelcomeMessage() {
         <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>I am learning about JSX!</p>
+=======
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+>>>>>>> 43e6ba5 (Added WelcomeMessage component)
         </div>
     );
 }
 
+<<<<<<< HEAD
 export default WelcomeMessage;
+=======
+export default App;
+>>>>>>> 43e6ba5 (Added WelcomeMessage component)
