46008ee783cedda88ac8de951378f4c0a1ab579c
issam-os <issam.bouyoussef94@gmail.com>
2025-02-20 21:53:59 +0100

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -10,3 +10,16 @@ function WelcomeMessage() {
 }
 
 export default WelcomeMessage;
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage'; 
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default App;
+ 
