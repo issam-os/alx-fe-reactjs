e96051d7bdd903fec995cd6cdeef1bee12504b87
issam-os <issam.bouyoussef94@gmail.com>
2025-03-03 15:55:26 +0000

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+  return (
+    <main style={{ padding: "20px", fontSize: "18px", lineHeight: "1.5", textAlign: "center" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
