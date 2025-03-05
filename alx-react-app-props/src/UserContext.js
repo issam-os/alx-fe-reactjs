d48958d9c43c18734f162b7a4749bac0ce350825
issam-os <issam.bouyoussef94@gmail.com>
2025-03-05 23:22:10 +0000

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const UserContext = createContext(null);
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState({ name: "Jane Doe", email: "jane.doe@example.com" });
+
+  return (
+    <UserContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
