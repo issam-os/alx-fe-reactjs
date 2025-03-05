b2dfd5a838c852556a8e7109f10970958c176c03
issam-os <issam.bouyoussef94@gmail.com>
2025-03-05 23:37:37 +0000

    Update UserDetails.jsx

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,1 +1,15 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
+function UserDetails() {
+  const { userData } = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Name: {userData.name}</h2>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
