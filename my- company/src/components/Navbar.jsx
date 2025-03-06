d77bc2639d71634b5deffb84570b2117067a65e0
issam-os <issam.bouyoussef94@gmail.com>
2025-03-06 00:08:01 +0000

    Update Navbar.jsx

--- my- company/src/components/Navbar.jsx
+++ my- company/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: 'black', color: 'white' }}>
-      <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', margin: '10px' }}>About</Link>
-      <Link to="/services" style={{ color: 'white', margin: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white', margin: '10px' }}>Contact</Link>
+    <nav style={{ background: "#333", padding: "10px" }}>
+      <Link to="/" style={{ color: "white", margin: "10px" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", margin: "10px" }}>About</Link>
+      <Link to="/services" style={{ color: "white", margin: "10px" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", margin: "10px" }}>Contact</Link>
     </nav>
   );
 }
