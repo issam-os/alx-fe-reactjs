f05fb5242655f27d3501977370be1062a314ce70
issam-os <issam.bouyoussef94@gmail.com>
2025-03-13 17:05:59 +0000

    Create RecipeDetails.js

---
+++ recipe-sharing-app/src/components/RecipeDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRecipeStore } from '../store/recipeStore';
+import { useNavigate } from 'react-router-dom';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
+  const navigate = useNavigate();
+
+  if (!recipe) return <div>Recipe not found.</div>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipeId} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
