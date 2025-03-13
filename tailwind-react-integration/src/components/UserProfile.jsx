e306ff02e17e0ce1c45d9363025b13db909a0a18
issam-os <issam.bouyoussef94@gmail.com>
2025-03-13 18:40:31 +0000

    Create UserProfile.jsx

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-10 rounded-lg 
+      shadow-lg hover:shadow-xl text-center transition-transform duration-300 ease-in-out hover:scale-110">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
+      />
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
