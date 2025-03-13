ece16dbd2b74f7ca6546882a3961013fb7bfff4f
issam-os <issam.bouyoussef94@gmail.com>
2025-03-13 18:44:46 +0000

    Create tailwind.config.js

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Remove unused styles in production
+  darkMode: false, // You can change this to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
