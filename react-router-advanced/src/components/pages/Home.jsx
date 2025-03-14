94a4b85e19cd9f8ffa0c966e0456416fca00e203
issam-os <issam.bouyoussef94@gmail.com>
2025-03-14 16:47:47 +0000

    Create Home.jsx

---
+++ react-router-advanced/src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link to="/profile">Go to Profile</Link>
+    <br />
+    <Link to="/blog/1">View Blog Post 1</Link>
+  </div>
+);
+
+export default Home;
