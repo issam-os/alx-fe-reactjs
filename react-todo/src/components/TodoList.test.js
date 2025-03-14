ba3ed9a67b8d47568dae61c25de7d197a07ae612
issam-os <issam.bouyoussef94@gmail.com>
2025-03-14 17:05:16 +0000

    Create TodoList.test.js

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders TodoList component", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Todo List")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const todoInput = screen.getByPlaceholderText("Add a todo...");
+  const addButton = screen.getByText("Add");
+
+  fireEvent.change(todoInput, { target: { value: "New Task" } });
+  fireEvent.click(addButton);
+
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText("Delete")[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
