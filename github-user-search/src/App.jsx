60148da8025e77efc37bcf20b09c2fcbf82a2b13
issam-os <issam.bouyoussef94@gmail.com>
2025-03-28 15:20:50 +0000

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import UserCard from "./components/UserCard";
-import { fetchGitHubUser } from "./services/githubAPI";
+import { fetchUserData } from "./services/githubService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const handleSearch = async (username) => {
+    setLoading(true);
+    setError(null);
+    setUser(null);
+
     try {
-      setError(null);
-      const data = await fetchGitHubUser(username);
+      const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
-      setError("User not found!");
-      setUser(null);
+      setError("Looks like we can't find the user");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -22,6 +27,7 @@ function App() {
     <div className="flex flex-col items-center p-4">
       <h1 className="text-2xl font-bold">GitHub User Search</h1>
       <SearchBar onSearch={handleSearch} />
+      {loading && <p className="text-gray-600 mt-2">Loading...</p>}
       {error && <p className="text-red-500 mt-2">{error}</p>}
       {user && <UserCard user={user} />}
     </div>
